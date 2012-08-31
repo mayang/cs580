5ea02c8cf638bc2076b611c0702053475590fe31
may ang <mayday815@gmail.com>
2012-08-30 23:07:43 -0700

    initial commit

---
+++ HW1/disp.cpp
@@ -0,0 +1,89 @@
+/*   CS580 HW   */
+#include    "stdafx.h"  
+#include	"Gz.h"
+#include	"disp.h"
+
+
+int GzNewFrameBuffer(char** framebuffer, int width, int height)
+{
+/* create a framebuffer:
+ -- allocate memory for framebuffer : (sizeof)GzPixel x width x height
+ -- pass back pointer 
+*/
+	// Pass by reference? * here?
+	//*framebuffer = (char*) malloc(sizeof(GzPixel) * width * height);
+	//if (*framebuffer == NULL) {
+	//	return GZ_FAILURE;
+	//}
+
+	return GZ_SUCCESS;
+}
+
+int GzNewDisplay(GzDisplay	**display, GzDisplayClass dispClass, int xRes, int yRes)
+{
+
+/* create a display:
+  -- allocate memory for indicated class and resolution
+  -- pass back pointer to GzDisplay object in display
+*/
+	//*display = malloc(sizeof(GzDisplay));
+
+	return GZ_SUCCESS;
+}
+
+
+int GzFreeDisplay(GzDisplay	*display)
+{
+/* clean up, free memory */
+	return GZ_SUCCESS;
+}
+
+
+int GzGetDisplayParams(GzDisplay *display, int *xRes, int *yRes, GzDisplayClass	*dispClass)
+{
+/* pass back values for an open display */
+	return GZ_SUCCESS;
+}
+
+
+int GzInitDisplay(GzDisplay	*display)
+{
+/* set everything to some default values - start a new frame */
+	return GZ_SUCCESS;
+}
+
+
+int GzPutDisplay(GzDisplay *display, int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
+{
+/* write pixel values into the display */
+	return GZ_SUCCESS;
+}
+
+
+int GzGetDisplay(GzDisplay *display, int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
+{
+	/* pass back pixel value in the display */
+	/* check display class to see what vars are valid */
+	return GZ_SUCCESS;
+}
+
+
+int GzFlushDisplay2File(FILE* outfile, GzDisplay *display)
+{
+
+	/* write pixels to ppm file based on display class -- "P6 %d %d 255\r" */
+
+	return GZ_SUCCESS;
+}
+
+int GzFlushDisplay2FrameBuffer(char* framebuffer, GzDisplay *display)
+{
+
+	/* write pixels to framebuffer: 
+		- Put the pixels into the frame buffer
+		- Caution: store the pixel to the frame buffer as the order of blue, green, and red 
+		- Not red, green, and blue !!!
+	*/
+
+	return GZ_SUCCESS;
+}
