0829524465217d64cb54810ce5da36614053aefa
may ang <mayday815@gmail.com>
2012-09-26 21:24:41 -0700

    hw3 initial commit

---
+++ HW3/rend.cpp
@@ -0,0 +1,153 @@
+/* CS580 Homework 3 */
+
+#include	"stdafx.h"
+#include	"stdio.h"
+#include	"math.h"
+#include	"Gz.h"
+#include	"rend.h"
+
+
+int GzRotXMat(float degree, GzMatrix mat)
+{
+// Create rotate matrix : rotate along x axis
+// Pass back the matrix using mat value
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRotYMat(float degree, GzMatrix mat)
+{
+// Create rotate matrix : rotate along y axis
+// Pass back the matrix using mat value
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRotZMat(float degree, GzMatrix mat)
+{
+// Create rotate matrix : rotate along z axis
+// Pass back the matrix using mat value
+
+	return GZ_SUCCESS;
+}
+
+
+int GzTrxMat(GzCoord translate, GzMatrix mat)
+{
+// Create translation matrix
+// Pass back the matrix using mat value
+
+	return GZ_SUCCESS;
+}
+
+
+int GzScaleMat(GzCoord scale, GzMatrix mat)
+{
+// Create scaling matrix
+// Pass back the matrix using mat value
+
+	return GZ_SUCCESS;
+}
+
+
+//----------------------------------------------------------
+// Begin main functions
+
+int GzNewRender(GzRender **render, GzRenderClass renderClass, GzDisplay	*display)
+{
+/*  
+- malloc a renderer struct 
+- keep closed until all inits are done 
+- setup Xsp and anything only done once 
+- span interpolator needs pointer to display 
+- check for legal class GZ_Z_BUFFER_RENDER 
+- init default camera 
+*/ 
+
+	return GZ_SUCCESS;
+
+}
+
+
+int GzFreeRender(GzRender *render)
+{
+/* 
+-free all renderer resources
+*/
+	return GZ_SUCCESS;
+}
+
+
+int GzBeginRender(GzRender *render)
+{
+/*  
+- set up for start of each frame - clear frame buffer 
+- compute Xiw and projection xform Xpi from camera definition 
+- init Ximage - put Xsp at base of stack, push on Xpi and Xiw 
+- now stack contains Xsw and app can push model Xforms if it want to. 
+*/ 
+	return GZ_SUCCESS;
+}
+
+int GzPutCamera(GzRender *render, GzCamera *camera)
+{
+/*
+- overwrite renderer camera structure with new camera definition
+*/
+	return GZ_SUCCESS;	
+}
+
+int GzPushMatrix(GzRender *render, GzMatrix	matrix)
+{
+/*
+- push a matrix onto the Ximage stack
+- check for stack overflow
+*/
+	return GZ_SUCCESS;
+}
+
+int GzPopMatrix(GzRender *render)
+{
+/*
+- pop a matrix off the Ximage stack
+- check for stack underflow
+*/
+	return GZ_SUCCESS;
+}
+
+
+int GzPutAttribute(GzRender	*render, int numAttributes, GzToken	*nameList, 
+	GzPointer	*valueList) /* void** valuelist */
+{
+/*
+- set renderer attribute states (e.g.: GZ_RGB_COLOR default color)
+- later set shaders, interpolaters, texture maps, and lights
+*/
+	
+	return GZ_SUCCESS;
+}
+
+int GzPutTriangle(GzRender	*render, int numParts, GzToken *nameList, 
+				  GzPointer	*valueList)
+/* numParts : how many names and values */
+{
+/*  
+- pass in a triangle description with tokens and values corresponding to 
+      GZ_POSITION:3 vert positions in model space 
+- Xform positions of verts  
+- Clip - just discard any triangle with verts behind view plane 
+       - test for triangles with all three verts off-screen 
+- invoke triangle rasterizer  
+*/ 
+	return GZ_SUCCESS;
+}
+
+/* NOT part of API - just for general assistance */
+
+short	ctoi(float color)		/* convert float color to GzIntensity short */
+{
+  return(short)((int)(color * ((1 << 12) - 1)));
+}
+
